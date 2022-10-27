9d2ebc0a53cd32285822a57936b094835e7d602a
Maulik045 <maulikbhatt045@gmail.com>
2022-10-27 18:36:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
-public class HelloWorld 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Hello World!!!! This is a demo of Jenkins.....");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!! This is a demo of Jenkins....."); 
     }
 }
